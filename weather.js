4f0df919a5bcd537595e5cdc3534a1079cf97dfa
Brandon Piercy <b.piercy567@gmail.com>
2020-11-12 13:40:52 -0800

    code now fetches the previous 5 days worth of weather data and stores it inside of an array

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
 var resultsBox = document.getElementById("results");
 var userLat = "";
 var userLong = "";
@@ -29,11 +29,36 @@ function showPosition(position) {
     console.log(userLong);
     console.log(currentDT);
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+currentDT+"&appid=87438d87c7c61316f75aa6c75715e39a",
+        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+dateCodes[0]+"&appid=87438d87c7c61316f75aa6c75715e39a",
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        weatherData.push(response);
     });
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+dateCodes[1]+"&appid=87438d87c7c61316f75aa6c75715e39a",
+        method: "GET"
+    }).then(function(response) {
+        weatherData.push(response);
+    });
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+dateCodes[2]+"&appid=87438d87c7c61316f75aa6c75715e39a",
+        method: "GET"
+    }).then(function(response) {
+        weatherData.push(response);
+    });
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+dateCodes[3]+"&appid=87438d87c7c61316f75aa6c75715e39a",
+        method: "GET"
+    }).then(function(response) {
+        weatherData.push(response);
+    });
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+userLat+"&lon="+userLong+"&dt="+dateCodes[4]+"&appid=87438d87c7c61316f75aa6c75715e39a",
+        method: "GET"
+    }).then(function(response) {
+        weatherData.push(response);
+    });
+    console.log(weatherData);
 };
 
 function generateDateCodes() {
