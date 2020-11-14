015bf1c0f3f6aead0c401e7a0bd62c3d0302f32e
Kyle Gibson <kyle.gibson22@yahoo.com>
2020-11-14 14:51:32 -0800

    switched to nyt api

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://extract-news.p.rapidapi.com/v0/article?url=https%3A%2F%2Fwww.theverge.com%2F2020%2F4%2F17%2F21224728%2Fbill-gates-coronavirus-lies-5g-covid-19",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "37f3968791msh437719ecb19973ap14c303jsn24e90ef1fa69",
-		"x-rapidapi-host": "extract-news.p.rapidapi.com"
-	}
-};
+$(document).ready(function (){
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+var today = new Date();
+var month = today.getMonth();
+var year = today.getFullYear();
+var key = "xL7n7LjuM75890uTCBZ7aflm935l0FoW";
+var queryURL = `https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=58GJOcaZcxd80kUCOX40Few3k4A1sWCn`
+
+console.log(month)
+console.log(year)
+
+$.ajax({
+        url: queryURL,
+         method: "GET"
+        }).then(function(response) {
+          console.log(response);
+        });
+
 });
+
